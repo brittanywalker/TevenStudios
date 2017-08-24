570f325091ed6f8095cf6f6861204ba00a5b16b9
harryjacko <hjac660@aucklanduni.ac.nz>
2017-08-25 11:33:59 +1200

    Can now pass variables between form and controller

--- TevenStudiosBudgetTracker/TevenStudiosBudgetTracker/Controllers/HomeController.cs
+++ TevenStudiosBudgetTracker/TevenStudiosBudgetTracker/Controllers/HomeController.cs
@@ -25,10 +25,6 @@ namespace TevenStudiosBudgetTracker.Controllers
             data.Users = context.GetAllUsers();
             data.Managers = context.GetAllManagers();
 
-            List<List<Models.User>> usersAndManagers = new List<List<Models.User>>();
-            usersAndManagers.Add(context.GetAllUsers());
-            usersAndManagers.Add(context.GetAllManagers());
-
             return View(data);
         }
 
@@ -42,6 +38,27 @@ namespace TevenStudiosBudgetTracker.Controllers
         public IActionResult Error()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult GetDetails()
+        {
+            User umodel = new User();
+            umodel.Name = HttpContext.Request.Form["name"].ToString();
+            umodel.Email = HttpContext.Request.Form["email"].ToString();
+            umodel.ManagerId = Int32.Parse(HttpContext.Request.Form["manager"].ToString());
+            umodel.RoleId = Int32.Parse(HttpContext.Request.Form["role"].ToString());
+
+            ViewBag.Result = "Good job, " + umodel.Name + "was added!";
+
+            // Not sure if this is correct, but need to reload data some how
+            // Maybe have this as a method as might be used multiple times
+            UserContext context = HttpContext.RequestServices.GetService(typeof(TevenStudiosBudgetTracker.Models.UserContext)) as UserContext;
+            AdminViewData data = new AdminViewData();
+            data.Users = context.GetAllUsers();
+            data.Managers = context.GetAllManagers();
+
+            return View("Index", data);
         }
     }
 }
