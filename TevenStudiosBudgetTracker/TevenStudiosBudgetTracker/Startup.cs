527241646934c04b6f326c8ceedaabc7928ca8a0
hjadenz <hsam938@aucklanduni.ac.nz>
2017-08-24 23:20:27 +1200

    Add service

--- TevenStudiosBudgetTracker/TevenStudiosBudgetTracker/Startup.cs
+++ TevenStudiosBudgetTracker/TevenStudiosBudgetTracker/Startup.cs
@@ -31,6 +31,7 @@ namespace TevenStudiosBudgetTracker
             // Add framework services.
             services.AddMvc();
             services.Add(new ServiceDescriptor(typeof(UserContext), new UserContext(Configuration.GetConnectionString("DefaultConnection"))));
+            services.Add(new ServiceDescriptor(typeof(PendingRequestsContext), new PendingRequestsContext(Configuration.GetConnectionString("DefaultConnection"))));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
