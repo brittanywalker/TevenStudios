31bd752b54242d11655e41039a96f11fb518afd1
AngadNayyar <angadnayyar@hotmail.com>
2017-08-27 12:53:03 +1200

    Add DeleteUser to HomeController

--- TevenStudiosBudgetTracker/TevenStudiosBudgetTracker/Controllers/HomeController.cs
+++ TevenStudiosBudgetTracker/TevenStudiosBudgetTracker/Controllers/HomeController.cs
@@ -73,5 +73,16 @@ namespace TevenStudiosBudgetTracker.Controllers
 
             return View("Index", data);
         }
+		
+		public IActionResult DeleteUser(int UserID)
+        {
+            UserContext context = HttpContext.RequestServices.GetService(typeof(TevenStudiosBudgetTracker.Models.UserContext)) as UserContext;
+            int result = context.DeleteUserSQL(UserID);
+
+            AdminViewData data = new AdminViewData();
+            data.Users = context.GetAllUsers();
+            data.Managers = context.GetAllManagers();
+            return View("Index",data);
+        }
     }
 }
