8aae277f7abea547c61548fb146dc1bdd9704134
Libby713 <e_stevenson@xtra.co.nz>
2017-09-27 00:58:32 +1300

    Removed hard coded user id from home controller and submit pending request method in employee context

--- TevenStudiosBudgetTracker/TevenStudiosBudgetTracker/Models/EmployeeContext.cs
+++ TevenStudiosBudgetTracker/TevenStudiosBudgetTracker/Models/EmployeeContext.cs
@@ -395,7 +395,7 @@ namespace TevenStudiosBudgetTracker.Models
                 return list;
             }
 
-            public int SubmitPendingRequest(PendingRequest newRequest)
+            public int SubmitPendingRequest(PendingRequest newRequest, int userId)
             {
             using (MySqlConnection conn = getConnection())
             {
@@ -405,7 +405,7 @@ namespace TevenStudiosBudgetTracker.Models
                 //string startDate = "2001-09-11 08:45:00";		
                 string query;
                 
-                query = "insert into Transactions(UserId, Date, Description, Amount, StatusId) values('" + 1 + "','" + newRequest.Date + "','" + newRequest.Description +
+                query = "insert into Transactions(UserId, Date, Description, Amount, StatusId) values('" + userId + "','" + newRequest.Date + "','" + newRequest.Description +
                 "','" + newRequest.Cost + "','" + 0 + "')";  // this status of 0 is pending and should be refactored to be an global variable later 
                 
 
